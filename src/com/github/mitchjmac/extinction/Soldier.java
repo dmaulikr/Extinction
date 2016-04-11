611d8398f8d801aeb1c58567cf278e00e0be134c
Mitchell MacInnis <mmac242@gmail.com>
2016-04-11 17:49:39 -0500

    added military

---
+++ src/com/github/mitchjmac/extinction/Soldier.java
@@ -0,0 +1,5 @@
+package com.github.mitchjmac.extinction;
+
+class Soldier extends Occupation {
+    
+}
---
+++ src/com/github/mitchjmac/extinction/Tank.java
@@ -0,0 +1,5 @@
+package com.github.mitchjmac.extinction;
+
+class Tank extends Vehicle {
+    
+}
